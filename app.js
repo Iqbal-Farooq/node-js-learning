3edf1423bd4d0edf22139c732f2a4ed8c4d6d4a3
iqbal ahmad <iqbalahmad.dar@qalbit.com>
2024-10-19 11:34:09 +0530

    learning template engine

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-const http = require("http");
-const routes=require("./route")
-const server = http.createServer(routes);
-server.listen(3000);
+const path=require('path')
+const express=require('express')
+const bodyParser=require('body-parser')
+const adminData=require('./routes/admin')
+const shopRoutes=require('./routes/shop')
+
+const app=express();
+app.set('view engine','pug');
+app.set('views','views')
+app.use(bodyParser.urlencoded({extend:false}));
+app.use(express.static(path.join(__dirname,'public')))
+
+app.use('/admin',adminData.routes);
+app.use(shopRoutes);
+app.use((req,res,next)=>{
+    res.render('404',{pagetitle:'Not Found!'})
+    // res.status(404).sendFile(path.join(__dirname,'views','404.html'))
+})
+
+// const server = http.createServer(app);
+// server.listen(3000);
+app.listen(3000)
---
+++ routes/shop.js
@@ -0,0 +1,10 @@
+const path=require('path')
+const express=require('express')
+const router=express.Router()
+const adminData=require('./admin')
+router.get('/',(req,res,next)=>{
+    const products=adminData.products
+    res.render('shop',{prods:products,docTitle:"shop"})
+    // res.sendFile(path.join(__dirname,'..','views','shop.html'))
+})
+module.exports = router

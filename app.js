e53c9a166ce0c931108a276fe9c859dd0fd01bf8
iqbal ahmad <iqbalahmad.dar@qalbit.com>
2024-12-25 11:49:54 +0530

    monoose

--- app.js
+++ app.js
@@ -1,53 +1,70 @@
-const mongoose=require("mongoose")
-const path=require('path')
-const express=require('express')
-const bodyParser=require('body-parser')
-const adminRoutes=require('./routes/admin')
-const shopRoutes=require('./routes/shop')
-const ErrorController=require('./controllers/404')
+const mongoose = require("mongoose");
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const ErrorController = require("./controllers/error");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const authRouts = require("./routes/auth");
 
-const User =require('./models/user')
-const app=express();
-
+const User = require("./models/user");
+const MONGODB_URL = "mongodb://localhost:27017/shop";
+const app = express();
+const store =new MongoDBStore( {
+  uri: MONGODB_URL,
+  collection: "sessions",
+})
 // EJS
-app.set('view engine','ejs');
-app.set('views','views')
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.static(path.join(__dirname,'public')))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({ secret: "my secret", resave: false, saveUninitialized: false,store:store })
+);
 
 app.use((req, res, next) => {
-    User.findById('67664bb24ff07ce5966a18f4')
-        .then(user => {
-            req.user=user
-            
-            next();
-        })
-        .catch(err => console.log(err));
+  
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      console.log('user ',user)
+      req.user = user; 
+      next();
+    })
+    .catch((err) => console.log(err));
 });
 
 
-app.use('/admin',adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
-app.use(ErrorController.get404)
+app.use(authRouts);
+app.use(ErrorController.get404);
 
-mongoose.connect("mongodb://localhost:27017/shop").then(()=>{
-    User.findOne().then(user=>{
-        if(!user){
-            const user= new User({
-                name:'John',
-                email:"john@gmail.com",
-                cart:{
-                    items:[]
-                }
-            })
-            user.save();
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+    User.findOne()
+      .then((user) => {
+        if (!user) {
+          const user = new User({
+            name: "John",
+            email: "john@gmail.com",
+            cart: {
+              items: [],
+            },
+          });
+          user.save();
         }
-    }).catch()
-    
-    console.log('connected siuccessfully')
-    app.listen(7000)
-}).catch(err=>console.log('err',err))
-   
+      })
+      .catch();
 
-
+    console.log("connected siuccessfully");
+    app.listen(7000);
+  })
+  .catch((err) => console.log("err", err));
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,70 +1,86 @@
-const Product = require("../models/product");
+const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
   Product.find()
-    .then((product) => {
-      res.render("shop/product-list", {
-        prods: product,
-        pageTitle: "ALL Products",
-        path: "/products",
+    .then(products => {
+      console.log(products);
+      res.render('shop/product-list', {
+        prods: products,
+        pageTitle: 'All Products',
+        path: '/products',
+        isAuthenticated: req.session.isLoggedIn
       });
     })
-    .catch((err) => console.log(err));
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId)
-    .then((product) => {
-      res.render("shop/product-detail", {
+    .then(product => {
+      res.render('shop/product-detail', {
         product: product,
         pageTitle: product.title,
-        path: "/products",
+        path: '/products',
+        isAuthenticated: req.session.isLoggedIn
       });
     })
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
   Product.find()
-    .then((product) => {
-      res.render("shop/index", {
-        prods: product,
-        pageTitle: "shop",
-        path: "/",
+    .then(products => {
+      res.render('shop/index', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+        isAuthenticated: req.session.isLoggedIn
       });
     })
-    .catch((err) => console.log(err));
-};
-exports.getCart = (req, res, next) => {
-  req.user.populate('cart.items.productId').then(user=>{
-    const products=user.cart.items
-      res.render("shop/cart", {
-        pageTitle: "Your Cart ",
-        path: "/cart",
-        products: products,
-      });
-    }).catch(err=>console.log(err))
- 
-};
-exports.postCart = (req, res, next) => {
-  const prodId = req.body.productId;
-  Product.findById(prodId).then(product=>{
-     req.user.addToCart(product)
-   
-  }).then(resut=>{
-    console.log(resut)
-    res.redirect('/cart')
-  }).catch(err=>console.log(err))
-  
+    .catch(err => {
+      console.log(err);
+    });
 };
 
-exports.postCartDeletProduct = (req, res, next) => {
-  const prodId = req.body.productId;
-  console.log('Product ID to delete:', prodId);
-  req.user.removeFromCart(prodId).then(result=>{
-    console.log(result)
-    res.redirect('/cart')
-  }).catch(err=>console.log(err))
-  
+exports.getCart = (req, res, next) => {
+  req.user
+    .populate('cart.items.productId')
+    .then(user => {
+      console.log('user.cart.items',user.cart.items)
+      const products = user.cart.items;
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: products,
+        isAuthenticated: req.session.isLoggedIn
+      });
+    })
+    .catch(err => console.log(err));
 };
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId)
+    .then(product => {
+      return req.user.addToCart(product);
+    })
+    .then(result => {
+      console.log(result);
+      res.redirect('/cart');
+    });
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  req.user
+    .removeFromCart(prodId)
+    .then(result => {
+      res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
+};
+
+
